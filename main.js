fb659945cc7e2522f620cb7e01ebe0df2d1dfec9
Kyle Amoroso <kamoroso1994@gmail.com>
2025-02-14 03:43:13 -0500

    Add gallery feature with updated screenshots

--- main.js
+++ main.js
@@ -2,15 +2,11 @@ import {ColorPicker} from './color-picker/color-picker.js';
 import {HintBox} from './hint-box/hint-box.js';
 import {HistoryWidget} from './history-widget/history-widget.js';
 import {Nonogram} from './nonogram/nonogram.js';
+import {assertExists} from './utils/asserts.js';
 
 /** @import {Color} from './color-picker/color-picker.js'; */
 
 const MAX_UNDO_HISTORY = 50;
-const historyWidget = new HistoryWidget({
-  undoSelector: '#undo-action',
-  redoSelector: '#redo-action',
-  maxHistory: MAX_UNDO_HISTORY,
-});
 
 /** @type {readonly Color[]} */
 const PALETTE = [
@@ -26,25 +22,50 @@ const PALETTE = [
   cssColor: `var(--color-${color})`,
 }));
 
-const colorPicker = new ColorPicker({
-  slotSelector: '#color-picker',
-  clearColorSelector: '#clear-color',
-  clearAllSelector: '#clear-all',
-  palette: PALETTE,
-});
+function initializeNonogram() {
+  const historyWidget = new HistoryWidget({
+    undoSelector: '#undo-action',
+    redoSelector: '#redo-action',
+    maxHistory: MAX_UNDO_HISTORY,
+  });
 
-const hintBox = new HintBox({
-  slotSelector: '#hint-box',
-  checkboxSelector: '#hints-setting',
-});
+  const colorPicker = new ColorPicker({
+    slotSelector: '#color-picker',
+    clearColorSelector: '#clear-color',
+    clearAllSelector: '#clear-all',
+    palette: PALETTE,
+  });
 
-void new Nonogram({
-  slotSelector: '#nonogram-game',
-  dimensionsSelector: '#dimensions-select',
-  difficultySelector: '#difficulty-select',
-  restartSelector: '#play-game',
-  submitSelector: '#submit-answer',
-  historyWidget,
-  colorPicker,
-  hintBox,
-});
+  const hintBox = new HintBox({
+    slotSelector: '#hint-box',
+    checkboxSelector: '#hints-setting',
+  });
+
+  void new Nonogram({
+    slotSelector: '#nonogram-game',
+    dimensionsSelector: '#dimensions-select',
+    difficultySelector: '#difficulty-select',
+    restartSelector: '#play-game',
+    submitSelector: '#submit-answer',
+    historyWidget,
+    colorPicker,
+    hintBox,
+  });
+}
+
+function initializeGallery() {
+  if (!document.fullscreenEnabled) return;
+
+  const gallery = assertExists(document.getElementById('gallery'));
+  gallery.addEventListener('click', (event) => {
+    if (document.fullscreenElement) return;
+
+    const element = /** @type {!HTMLElement} */ (event.target);
+    if (!element.matches('img')) return;
+
+    element.requestFullscreen({navigationUI: 'show'});
+  });
+}
+
+initializeNonogram();
+initializeGallery();
